8b7696fbd5fc6871fd5e49ec42d423f9d7e0acd8
aininazmn <124043846+aininazmn@users.noreply.github.com>
2024-02-21 22:24:10 +0000

    resolve email issue

--- app/__init__.py
+++ app/__init__.py
@@ -2,8 +2,6 @@ from flask import Flask
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
-from .models import db, login_manager
-from .views import main_blueprint
 
 db = SQLAlchemy()
 login_manager = LoginManager()
@@ -11,7 +9,6 @@ migrate = Migrate()
 
 def create_app():
     app = Flask(__name__)
-
     app.config.from_object('config.ConfigClass')  # refer the class in config.py
 
     # init extensions
@@ -19,8 +16,17 @@ def create_app():
     login_manager.init_app(app)
     migrate.init_app(app, db)
 
+    login_manager.login_view = 'main.login'  # endpoint for login page
+
     # import and register blueprints
     from .views import main_blueprint
     app.register_blueprint(main_blueprint)
 
     return app
+
+# load a user from db
+@login_manager.user_loader
+def load_user(user_id):
+    from .models import User
+    # will return user object based on user id
+    return User.query.get(int(user_id))
