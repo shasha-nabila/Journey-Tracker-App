24c0b8905756e25182e38111cb3d72e933e5db44
aininazmn <sc22nabm@leeds.ac.uk>
2024-03-07 11:36:21 +0000

    Implement confirm password

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, EmailField
-from wtforms.validators import DataRequired, Email, Length # validators
+from wtforms.validators import DataRequired, Email, Length, EqualTo # validators
 
 # define form for login process
 class LoginForm(FlaskForm):
@@ -13,4 +13,5 @@ class RegistrationForm(FlaskForm):
     email = EmailField('Email', validators=[DataRequired(), Email()])
     username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)])
     password = PasswordField('Password', validators=[DataRequired()])
+    confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password', message= 'Password does not match')])
     submit = SubmitField('Register')
