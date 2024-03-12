f7e91ecf555e1e172550d047845a5d2e525f84c8
shashanabila <sc22snba@leeds.ac.uk>
2024-03-12 23:15:20 +0000

    Delete all files

--- app/static/main.js
+++
@@ -1,7 +0,0 @@
-// Tooltip Initialization
-document.addEventListener("DOMContentLoaded", function () {
-    var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
-    var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
-        return new bootstrap.Tooltip(tooltipTriggerEl);
-    });
-});
--- team-project-team-13-main-4/db_create.py
+++
@@ -1,7 +0,0 @@
-from app import create_app, db
-
-app = create_app()
-
-# create db table within the app
-with app.app_context():
-    db.create_all()
