2b07f502faec343067019646573f4dfb675cec16
sc22cl <127819762+KadeHyunLee@users.noreply.github.com>
2024-03-21 09:43:21 +0000

    MapDatabase

--- app/utils.py
+++ app/utils.py
@@ -3,8 +3,11 @@ from .models import StripeSubscription
 from geopy.distance import geodesic
 from config import ConfigClass
 from werkzeug.utils import secure_filename
-from .models import StripeSubscription
+from .models import StripeSubscription,Location,Journey
 import random
+from datetime import datetime
+from .extensions import db
+from flask_login import current_user
 
 def is_valid_password(password_hash):
     # Password must have at least 1 capital letter, 1 numeric, and be at least 8 characters long
@@ -50,7 +53,7 @@ def calculate_projected_revenue(db):
     return projected_revenue
 
 def save_uploaded_file(file, upload_folder):
-
+    # test
     if not os.path.exists(upload_folder):
         os.makedirs(upload_folder)
 
@@ -89,11 +92,25 @@ def info_parse_gpx(file_path):
         return info
 
 def create_and_append_csv(file_path, header, data):
- 
-    with open(file_path, 'w', newline='') as csvfile:
+    
+    current_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+
+    file_exists = os.path.exists(file_path)
+    
+    with open(file_path, 'a', newline='') as csvfile:  
         writer = csv.writer(csvfile)
-        writer.writerow(header)
-        writer.writerows(data)
+        
+        if not file_exists:
+            writer.writerow(header)
+        
+        combined_row = []
+        for row in data:
+            combined_row.extend(row)  
+        
+        combined_row.append(current_time)
+
+    
+        writer.writerow(combined_row)
 
 def create_map_html(coordinates): 
     m = folium.Map(location=coordinates[0], zoom_start=17)
@@ -109,3 +126,41 @@ def calculate_distance(point1, point2):
     coords_1 = (point1['latitude'], point1['longitude'])
     coords_2 = (point2['latitude'], point2['longitude'])
     return geodesic(coords_1, coords_2).meters
+
+def uplaod_journey_database(user_id, total_distance, upload_time):
+       
+    new_journey = Journey(
+        user_id=user_id,
+        total_distance=total_distance,
+        upload_time=upload_time
+        )
+        
+    db.session.add(new_journey)
+        
+    db.session.commit()
+        
+    return new_journey
+
+def add_locations_from_csv(csv_file_path_1,csv_file_path_2, user_id):
+
+    with open(csv_file_path_1, 'r') as csvfile:
+        reader = csv.DictReader(csvfile)
+        first_row = next(reader)  
+        total_distance = float(first_row['distance'])  
+        upload_time = datetime.strptime(first_row['upload_time'], '%Y-%m-%d %H:%M:%S')  # upload_time 값을 얻습니다.
+
+    new_journey = uplaod_journey_database(user_id, total_distance, upload_time)
+
+    with open(csv_file_path_2, 'r') as csvfile:
+        reader = csv.DictReader(csvfile)  
+        for row in reader:
+            
+            location = Location(
+                journey_id=new_journey.id,  
+                init_latitude=float(row['latitude_init']),
+                init_longitude=float(row['longitude_init']),
+                goal_latitude=float(row['latitude_goal']),
+                goal_longitude=float(row['longitude_goal'])
+            )
+            db.session.add(location)  
+        db.session.commit()  
